3f39e92496c02ab943a520422fdf4dd7bd420bff
dev <Devendra.Meena@gigatorb.com>
2023-09-25 12:56:12 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import BrandSection from "./Componets/BrandSection"
+import HeroSection from "./Componets/HeroSection"
+import ProductCard from "./Componets/ProductCard"
+import ServiceApp from "./Componets/ServiceApp"
+import FeatureCard from "./Componets/featureCard"
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <HeroSection/>
+      <ProductCard/>
+      <BrandSection/>
+      <FeatureCard/>
+      <ServiceApp/>
+
+
+      
+      </div>
+  )
+}
+
+export default App
